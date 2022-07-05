f8ebfba67b32ab542c626b124235404c256149aa
project-sbc <78889381+project-sbc@users.noreply.github.com>
2022-07-05 06:57:20 -0400

    Touchups

--- Overlay.xaml.cs
+++ Overlay.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using Power_Control_Panel.PowerControlPanel.Classes.ChangeTDP;
+using Power_Control_Panel.PowerControlPanel.PageComponents;
 
 namespace Power_Control_Panel
 {
@@ -24,6 +25,8 @@ namespace Power_Control_Panel
         WindowSinker sinker;
         public Overlay()
         {
+            InitializeComponent();
+
             sinker = new WindowSinker(this);
             sinker.Sink();
             InitializeComponent();
@@ -31,6 +34,8 @@ namespace Power_Control_Panel
             this.Topmost = true;
             this.WindowStyle = WindowStyle.None;
             this.AllowsTransparency = true;
+
+            arrangeStackPanel();
         }
 
 
@@ -47,6 +52,28 @@ namespace Power_Control_Panel
 
         private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+
+        }
+        void arrangeStackPanel()
+        {
+            Page[] page;
+            Frame[] frame = new Frame[3];
+            frame[0] = frame0;
+            frame[1] = frame1;
+            frame[2] = frame2;
+            page = new Page[3];
+            int i = 0;
+
+            //if (Properties.Settings.Default.enableSystem) { page[i] = new HomeSystem(); i = i + 1; }
+            if (Properties.Settings.Default.enableTDP) { page[i] = new OverlayTDP(); i = i + 1; }
+            //if (Properties.Settings.Default.enableCPU) { page[i] = new HomeCPU(); i = i + 1; }
+
+            i = 0;
+            while (page[i] is not null)
+            {
+                frame[i].Navigate(page[i]);
+                i = i + 1;
+            }
 
         }
     }
